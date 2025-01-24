d20689b11a3e36eba3afe4c714ab3f41fceb60c4
BlueMoonJune <bluemoonjune0403@gmail.com>
2025-01-24 00:27:22 -0800

    Basket Mixin

--- src/main/java/bluemoonjune/baskettipping/mixin/BasketMixin.java
+++ src/main/java/bluemoonjune/baskettipping/mixin/BasketMixin.java
@@ -1,27 +1,29 @@
 package bluemoonjune.baskettipping.mixin;
 
-import net.minecraft.core.block.BlockBasket;
-import net.minecraft.core.block.BlockTileEntity;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockLogic;
+import net.minecraft.core.block.BlockLogicBasket;
+import net.minecraft.core.block.entity.TileEntityBasket;
 import net.minecraft.core.block.material.Material;
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = BlockLogicBasket.class)
-public abstract class BasketMixin extends BlockTileEntity {
+public abstract class BasketMixin extends BlockLogic {
 
-	public BasketMixin(String key, int id, Material material) {
-		super(key, id, material);
+
+	public BasketMixin(Block<?> block, Material material) {
+		super(block, material);
 	}
 
-	@Inject(
-		method = "onBlockRightClicked",
-		at = @At("HEAD"),
-		remap = false,
-		cancellable = true
-	)
-	public void tipBasket(CallbackInfo ci) {
-		ci.cancel();
+	@Override
+	public boolean onBlockRightClicked(World world, int x, int y, int z, Player entityplayer, Side side, double xPlaced, double yPlaced) {
+		entityplayer.sendMessage("mwah ha ha i have taken over your basket");
+		return false;
 	}
 }
