dc4492f9bc17699704b81ddd8a2411073beb2c4c
BlueMoonJune <bluemoonjune0403@gmail.com>
2025-01-24 00:12:37 -0800

    awjwdkajsd

--- src/main/java/bluemoonjune/baskettipping/BasketTipping.java
+++ src/main/java/bluemoonjune/baskettipping/BasketTipping.java
@@ -3,17 +3,16 @@ package bluemoonjune.baskettipping;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.helper.BlockBuilder;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
-    public static final String MOD_ID = "examplemod";
+public class BasketTipping implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+    public static final String MOD_ID = "baskettipping";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("BasketTipping initialized.");
     }
 
 	@Override
--- src/main/java/bluemoonjune/baskettipping/mixin/BasketMixin.java
+++ src/main/java/bluemoonjune/baskettipping/mixin/BasketMixin.java
@@ -1,4 +1,27 @@
 package bluemoonjune.baskettipping.mixin;
 
-public class BasketMixin {
+import net.minecraft.core.block.BlockBasket;
+import net.minecraft.core.block.BlockTileEntity;
+import net.minecraft.core.block.material.Material;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = BlockLogicBasket.class)
+public abstract class BasketMixin extends BlockTileEntity {
+
+	public BasketMixin(String key, int id, Material material) {
+		super(key, id, material);
+	}
+
+	@Inject(
+		method = "onBlockRightClicked",
+		at = @At("HEAD"),
+		remap = false,
+		cancellable = true
+	)
+	public void tipBasket(CallbackInfo ci) {
+		ci.cancel();
+	}
 }
